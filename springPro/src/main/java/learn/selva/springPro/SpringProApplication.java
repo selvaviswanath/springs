2c3d7c027b77732577af00264655e1cab2cf6c87
selvaviswanath <selvaviswanath055@gmail.com>
2022-08-06 04:11:05 +0530

    first commit

---
+++ springPro/src/main/java/learn/selva/springPro/SpringProApplication.java
@@ -0,0 +1,13 @@
+package learn.selva.springPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProApplication.class, args);
+	}
+
+}
---
+++ springPro/src/test/java/learn/selva/springPro/SpringProApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.selva.springPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
